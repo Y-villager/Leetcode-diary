51d51f1fbf1335ad0a973653d5a8ef74e8680869
Y-villager <59049169+Y-villager@users.noreply.github.com>
2022-07-21 01:45:31 +0800

    20220721

--- code/二分查找.java
+++ code/二分查找.java
@@ -7,5 +7,18 @@ package code;
  * @date: 2022年07月20日 23:26
  */
 public class 二分查找 {
+    public static void main(String[] args){
+        int[] nums ={ -1, 0,3,5,9,12};
+        int target = 9;
+        int res = search(nums, target);
+        System.out.println(res);
+    }
 
+    public static int search(int[] nums, int target){
+        int left =0,right=nums.length-1;
+        while (left< right){
+
+        }
+        return -1;
+    }
 }
