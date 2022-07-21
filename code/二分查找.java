5e21b0fced01fb3d64a01f8e86e8c142fcfc7902
Y-villager <59049169+Y-villager@users.noreply.github.com>
2022-07-21 10:01:13 +0800

    20220721

--- code/二分查找.java
+++ code/二分查找.java
@@ -7,17 +7,26 @@ package code;
  * @date: 2022年07月20日 23:26
  */
 public class 二分查找 {
-    public static void main(String[] args){
-        int[] nums ={ -1, 0,3,5,9,12};
+    public static void main(String[] args) {
+        int[] nums = {-1, 0, 3, 5, 9, 12, 99};
         int target = 9;
         int res = search(nums, target);
         System.out.println(res);
     }
 
-    public static int search(int[] nums, int target){
-        int left =0,right=nums.length-1;
-        while (left< right){
-
+    public static int search(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            // 取有序数组中间值
+            int mid = (left + right) / 2;
+            int num = nums[mid];
+            if (target == num) {
+                return mid;
+            } else if (target > num) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
         }
         return -1;
     }
