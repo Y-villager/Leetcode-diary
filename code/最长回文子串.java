3925582b106f9c1b5c844ddd3fee82461ab244aa
Y-villager <59049169+Y-villager@users.noreply.github.com>
2022-07-29 23:06:17 +0800

    Update 最长回文子串.java

--- code/最长回文子串.java
+++ code/最长回文子串.java
@@ -7,35 +7,47 @@ package code;
  * @date: 2022年07月22日 18:04
  */
 public class 最长回文子串 {
-    public static String longestPalindrome(String s) {
+
+    // 预处理
+    private static String preprocess(){
+        String res = "";
+        return res;
+    }
+
+    // Manacher 算法（马拉车算法）
+    private static String longestPalindrome2(String s) {
         String ans = "";
+        return ans;
+    }
+
+    // 动态规划
+    public static String longestPalindrome1(String s) {
+        StringBuilder ans = new StringBuilder();
         int n = s.length();
         int[][] dp = new int[n][n];
-        int[][] sub = new int[1][2];
         int max = 0;
+        int begin = 0;
         for (int j = 0; j < n; j++) {
             for (int i = 0; i <= j; i++) {
                 if (s.charAt(i) == s.charAt(j) && (j - i < 2 || dp[i + 1][j - 1] !=0)) {
                     dp[i][j] = j - i + 1;
                     if (dp[i][j] > max) {
                         max = dp[i][j];
-                        sub[0][0] = i;
-                        sub[0][1] = j;
+                        begin = i;
                     }
                 }
             }
         }
         回文子串.printDP(dp);
-        for (int i = sub[0][0]; i <= sub[0][1]; i++) {
-            ans += s.charAt(i);
-        }
 
-        return ans;
+        return s.substring(begin, max+begin);
     }
 
     public static void main(String[] args) {
         String s = "babad";
 
-        System.out.println(longestPalindrome(s));
+        System.out.println(longestPalindrome2(s));
     }
+
+
 }
